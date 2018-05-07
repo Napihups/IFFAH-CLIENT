f954ba8fd994fde4735bf80e9c43c4b8465533e0
Napihup <programminghup@gmail.com>
2018-05-07 15:45:37 +0800

    adding datepicker package in npm and layout for paycheck form submit...

--- src/app/user/components/leaf-house/leaf-house.component.ts
+++ src/app/user/components/leaf-house/leaf-house.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-leaf-house',
   templateUrl: './leaf-house.component.html',
   styleUrls: ['./leaf-house.component.css']
 })
-export class LeafHouseComponent implements OnInit {
+export class LeafHouseComponent implements OnInit, AfterViewInit {
+
+
+	//------- DOM ---------------------------//
+	@ViewChild('paycheckForm') paycheckForm
+	//---------------------------------------//
+
+	//------------ DOM STATE -----------------
+	paycheckFormDisplay:boolean = false;
+	//---------------------------------------//
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  ngAfterViewInit(){
+  	if(!this.paycheckFormDisplay){
+  		this.paycheckForm.nativeElement.className = 'paycheck-form' ;
+  	}
+  }
+
+
+
+  //------------ DOM FUNCTIONS ------------------------------//
+  paycheckFormClick(){
+  	this.paycheckFormDisplay = !this.paycheckFormDisplay;
+  	if(!this.paycheckFormDisplay){
+  		this.paycheckForm.nativeElement.className = 'paycheck-form' ;
+  	} else {
+  		this.paycheckForm.nativeElement.className = 'paycheck-form paycheck-form-display' ;
+  	}
+  }
+
+
 
 }
