053b1882c75b67e55be3dc450b310217eb917e12
Napihup <programminghup@gmail.com>
2018-05-15 10:36:00 +0800

    Form layout for finance setup and basic user-flow added

--- src/app/user/components/f-setup/f-setup.component.ts
+++ src/app/user/components/f-setup/f-setup.component.ts
@@ -1,20 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-f-setup',
   templateUrl: './f-setup.component.html',
   styleUrls: ['./f-setup.component.css']
 })
-export class FSetupComponent implements OnInit {
+export class FSetupComponent implements OnInit, AfterViewInit {
 
 
 /** ------------------STATES VARIABLES -------------------------*/
-typeSelected: string = 'yearly'
+typeSelected: string = 'W'
+@ViewChild('sBtnW') btnWeek;
+@ViewChild('sBtnM') btnMonth;
+@ViewChild('sBtnY') btnYear;
 
 /**--------------------------------------------------------------*/
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(){
+  	this.toggleSelectButton();
+  }
+
+
+
+
+  /** ------------ FUNCTIONS ---------------------------------------*/
+  selectBtnClick(code){
+  	this.typeSelected = code;
+  	this.toggleSelectButton();
+  }
+
+  toggleSelectButton(){
+  	switch(this.typeSelected) {
+  		case 'W': {
+  			this.btnWeek.nativeElement.classList.add('btn-selected');
+  			this.btnMonth.nativeElement.classList.remove('btn-selected');
+  			this.btnYear.nativeElement.classList.remove('btn-selected');
+  			break;
+  		} 
+  		case 'M': {
+  			this.btnWeek.nativeElement.classList.remove('btn-selected');
+  			this.btnMonth.nativeElement.classList.add('btn-selected');
+  			this.btnYear.nativeElement.classList.remove('btn-selected');
+  			break;
+  		} 
+  		case 'Y': {
+  			this.btnWeek.nativeElement.classList.remove('btn-selected');
+  			this.btnMonth.nativeElement.classList.remove('btn-selected');
+  			this.btnYear.nativeElement.classList.add('btn-selected');
+  			break;
+  		} 
+
+  	}
   }
 
 }
